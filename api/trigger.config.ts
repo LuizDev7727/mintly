ac659d0f201657775463dce6efad675b8e373750
LuizDev7727 <luiz.antonioq2003@gmail.com>
2026-09-02 17:34:17 -0300

    chore: change way to get env value on triggge build

--- api/trigger.config.ts
+++ api/trigger.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@trigger.dev/sdk";
 import { ffmpeg } from "@trigger.dev/build/extensions/core";
 import { syncEnvVars } from "@trigger.dev/build/extensions/core";
 import { InfisicalSDK } from "@infisical/sdk";
-import { env } from "@/env.ts";
 
 export default defineConfig({
   project: "proj_nzoodvxxkshyumjxnzmn",
@@ -31,13 +30,13 @@ export default defineConfig({
         const client = new InfisicalSDK();
 
         await client.auth().universalAuth.login({
-          clientId: env.INFISICAL_CLIENT_ID,
-          clientSecret: env.INFISICAL_CLIENT_SECRET,
+          clientId: process.env.INFISICAL_CLIENT_ID!,
+          clientSecret: process.env.INFISICAL_CLIENT_SECRET!,
         });
 
         const { secrets } = await client.secrets().listSecrets({
           environment: "prod",
-          projectId: env.INFISICAL_PROJECT_ID,
+          projectId: process.env.INFISICAL_PROJECT_ID!,
         });
 
         return secrets.map((secret) => ({
